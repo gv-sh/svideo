5d826b79f132534dcf30d8acdc9d740ad54cbbe7
0xf17 <grv@mathscapes.xyz>
2019-09-26 18:59:33 +0530

    omx test

---
+++ omx_test/videoplay.py
@@ -0,0 +1,11 @@
+from omxplayer.player import OMXPlayer
+from pathlib import Path
+from time import sleep
+
+VIDEO_PATH = Path("../tests/media/test_media_1.mp4")
+
+player = OMXPlayer(VIDEO_PATH)
+
+sleep(5)
+
+player.quit()
