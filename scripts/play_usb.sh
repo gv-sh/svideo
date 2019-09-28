b1f3b799d010bcabb9c762355878a14554171966
0xf17 <grv@mathscapes.xyz>
2019-09-28 20:15:22 +0530

    play locally

--- scripts/play_usb.sh
+++ scripts/play_usb.sh
@@ -1,2 +1,3 @@
 # omxplayer /media/usb/videos/01.mp4
-vlc /media/usb/videos/playlist.m3u --no-video-title -q
+# vlc /media/usb/videos/playlist.m3u --no-video-title -q
+vlc ~/videos/playlist.m3u --no-video-title -q
