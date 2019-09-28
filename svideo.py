8cf6a6d1999a3e4bbe0da5c1224b62e7503e4bd4
0xf17 <grv@mathscapes.xyz>
2019-09-28 18:20:07 +0530

    changing server back to laptop

--- svideo.py
+++ svideo.py
@@ -6,8 +6,8 @@ import sys
 import datetime 
 import os
 
-HOSTS               = ['192.168.0.100', '192.168.0.106']
-HOST                = HOSTS[1]
+HOSTS               = ['192.168.0.100', '192.168.0.105', '192.168.0.106']
+HOST                = HOSTS[0]
 PORT                = 8766
 MAX_CLIENTS         = 2
 LISTENING_TIME_OUT  = 1.0
