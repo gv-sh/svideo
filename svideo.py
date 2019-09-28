ba102478c4ecc7536781f3f5113845ce1015bc48
0xf17 <grv@mathscapes.xyz>
2019-09-28 19:50:55 +0530

    moved server for testing

--- svideo.py
+++ svideo.py
@@ -6,8 +6,7 @@ import sys
 import datetime 
 import os
 
-# HOSTS               = ['192.168.0.100', '192.168.0.105', '192.168.0.106']
-HOST                = 'rpi1.local'
+HOST                = 'rpi2.local'
 PORT                = 8766
 MAX_CLIENTS         = 2
 LISTENING_TIME_OUT  = 1.0
