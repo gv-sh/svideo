c8de42453436ba4f65cd38902c1567032e0a0656
0xf17 <grv@mathscapes.xyz>
2019-09-28 21:22:28 +0530

    added loop

--- scripts/play_usb.sh
+++ scripts/play_usb.sh
@@ -1,3 +1,3 @@
 # omxplayer /media/usb/videos/01.mp4
 # vlc /media/usb/videos/playlist.m3u --no-video-title -q
-vlc ~/videos/playlist.m3u --no-video-title -q
+vlc ~/videos/playlist.m3u --no-video-title -q --loop
