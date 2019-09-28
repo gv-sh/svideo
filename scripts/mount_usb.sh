e480087d367ae0410eed97e2a4a2de7f29f3d8ee
0xf17 <grv@mathscapes.xyz>
2019-09-28 18:04:28 +0530

    attempt connecting all possible usb devices

--- scripts/mount_usb.sh
+++ scripts/mount_usb.sh
@@ -1,1 +1,3 @@
-sudo mount /dev/sda1 /media/usb -o uid=pi,gid=pi
+sudo mount /dev/sda1 /media/usb -o uid=pi,gid=pi &
+sudo mount /dev/sdb1 /media/usb -o uid=pi,gid=pi &
+sudo mount /dev/sdc1 /media/usb -o uid=pi,gid=pi &
